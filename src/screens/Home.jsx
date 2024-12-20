6a56669cb3fefb65d0ca8c7889530cb37bda594b
Shreyashthaware2003 <shreyashthaware284@gmail.com>
2024-12-20 19:50:49 +0530

    fetched food items and categories and maintain the responsiveness

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,23 +1,71 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Card from '../components/Card'
+
+
 function Home() {
+
+    const [foodCat, setFoodCat] = useState([]);
+    const [foodItem, setFoodItem] = useState([]);
+
+    const loadData = async () => {
+        let response = await fetch("http://localhost:3000/api/foodData", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        response = await response.json();
+
+        setFoodItem(response[0]);
+        setFoodCat(response[1]);
+        // console.log(response[0], response[1]);
+    }
+
+    useEffect(() => {
+        loadData()
+    }, [])
+
+
+
     return (
         <>
             <Navbar />
             <div className='mt-32 max-w-[1280px] mx-auto'>
 
-                <div className='grid sm:grid-cols-3 md:grid-cols-5 items-center justify-center gap-8'>
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
+                <div className='grid  grid-cols-1 items-center justify-center gap-8'>
+
+                    {
+                        foodCat.length !== 0 ? (
+                            foodCat.map((data) => {
+                                return (
+                                    <div key={data._id} className='md:block flex flex-col justify-center items-center'>
+                                        <div className="text-xl m-3 ">{data.CategoryName}</div>
+                                        <hr />
+                                        <div className='grid sm:grid-cols-2 md:grid-cols-4 gap-8'>
+
+                                            {foodItem.length !== 0 ? foodItem.filter((item) => item.CategoryName === data.CategoryName
+                                            ).map(filterItems => {
+                                                return (
+                                                    <div key={filterItems._id}><Card></Card></div>
+                                                )
+                                            }) : (
+                                                <div>No such data</div>
+                                            )}
+                                        </div>
+                                    </div>
+                                );
+                            })
+                        ) : (
+                            <div>Failed to load data</div>
+                        )
+                    }
+
+                    {/* <Card /> */}
+
+
                 </div>
             </div>
             <Footer />
